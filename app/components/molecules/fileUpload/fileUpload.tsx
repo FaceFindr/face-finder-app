37ec039084530dc3813cd41d1ac2ab48f8b15bf4
Alexandre Martins <75785342+AleexMaartins@users.noreply.github.com>
2024-04-25 23:44:26 +0100

    saves the uploaded photos in a FormData() to be later processed as a background task

--- app/components/molecules/fileUpload/fileUpload.tsx
+++ app/components/molecules/fileUpload/fileUpload.tsx
@@ -4,16 +4,30 @@ import { useDropzone } from 'react-dropzone';
 import fileUploadStyle from './fileUploadStyle.module.css'
 import { ImFolderUpload } from 'react-icons/im';
 
-
-export type FileUploadProps ={
-    onUpload:(acceptedFiles:any)=>void;
+export type FileUploadProps = {
+    onUpload: (acceptedFiles: any) => void;
 }
-export default function FileUpload({onUpload}: FileUploadProps){
+
+export default function FileUpload({ onUpload }: FileUploadProps) {
     const { getRootProps, getInputProps } = useDropzone({
         accept: {
             'image/*': ['.jpeg', '.png']
         },
         onDrop: (acceptedFiles) => {
+            const formData = new FormData();
+            acceptedFiles.forEach((file) => {
+                formData.append('files', file);
+            });
+
+            fetch('/photo/albumId', {
+                method: 'POST',
+                body: formData
+            }).then(() => {
+                // navigate to another page here, or close the modal
+            }).catch((error) => {
+                console.error('Error:', error);
+            });
+
             onUpload(acceptedFiles);
         },
     });
@@ -21,10 +35,10 @@ export default function FileUpload({onUpload}: FileUploadProps){
     const otherAtt = { directory: "", webkitdirectory: "" };
 
     return (
-      <div {...getRootProps()} className={fileUploadStyle.dragArea} {...otherAtt}>
-        <input {...getInputProps()}  />
-        <ImFolderUpload style={{width:'50px', height:'50px'}} />
-        <p>Drag and drop files here or click to browse.</p>
-      </div>
+        <div {...getRootProps()} className={fileUploadStyle.dragArea} {...otherAtt}>
+            <input {...getInputProps()} />
+            <ImFolderUpload style={{width:'50px', height:'50px'}} />
+            <p>Drag and drop files here or click to browse.</p>
+        </div>
     );
 };
